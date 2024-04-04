25e470750f9264837743bd9c59ea4aaedb8c7caa
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-04 17:20:00 +0200

    feat: Implement WebscoketConfigurer

--- src/main/java/com/charlesmoswane/snake/config/WebSocketConfig.java
+++ src/main/java/com/charlesmoswane/snake/config/WebSocketConfig.java
@@ -1,4 +1,15 @@
 package com.charlesmoswane.snake.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+    }
 }
