41e51fbd4f2c26df80b81d55538641b515cef595
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-04 17:50:00 +0200

    feat: Implement SecondsCounterHandler

--- src/main/java/com/charlesmoswane/snake/handler/SecondsCounterHandler.java
+++ src/main/java/com/charlesmoswane/snake/handler/SecondsCounterHandler.java
@@ -1,6 +1,36 @@
 package com.charlesmoswane.snake.handler;
 
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class SecondsCounterHandler extends TextWebSocketHandler {
+
+    private static Set<WebSocketSession> sessions =
+            Collections.newSetFromMap(new ConcurrentHashMap<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception{
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+    public static void broadcast(String message){
+        sessions.forEach(session -> {
+            try {
+                session.sendMessage(new TextMessage(message));
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+    }
 }
