254e2d2527b8fb82565b17af4be648421aded3c0
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-04-07 17:10:00 +0200

    feat: Implement CounterScheduler

--- src/main/java/com/charlesmoswane/snake/scheduler/CounterScheduler.java
+++ src/main/java/com/charlesmoswane/snake/scheduler/CounterScheduler.java
@@ -1,4 +1,19 @@
 package com.charlesmoswane.snake.scheduler;
 
+import com.charlesmoswane.snake.handler.SecondsCounterHandler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
 public class CounterScheduler {
+
+    private int secondsCount = 0;
+
+    @Scheduled(fixedRate = 1000)
+    public void sendSecondsCount(){
+        secondsCount++;
+        SecondsCounterHandler.broadcast(String.valueOf(secondsCount));
+    }
 }
